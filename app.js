a60370e26668d6e405a4d682cefe03bf8343180f
SouganthikaS <56484155+SouganthikaS@users.noreply.github.com>
2020-10-18 17:20:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
-angular
-    .module('TaskListApp', [])
+var app = angular.module('TaskListApp', []);
+
+app.factory('myFactory', function () {
+    var displayFact;
+    var addMSG = function (msg) {
+        displayFact = '18IT008 Akash K' + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + '18IT089 Souganthika S';
+    }
+    return {
+        setMSG: function (msg) {
+            addMSG(msg);
+        },
+        getMSG: function () {
+            return displayFact;
+        }
+    };
+});
+app.controller('TaskListController', function ($scope, myFactory) {
+    $scope.val = "Task";
+    myFactory.setMSG("Tutlane (with Factory)");
+    $scope.textval = myFactory.getMSG();
+});
+
+app
     .controller('TaskListAddController', TaskListAddController)
     .controller('TaskListShowController', TaskListShowController)
     .service('TaskListService', TaskListService)
     .directive("testDirective", function () {
         return {
-          template: "<h1>Time Sectographer</h1>"
+            template: "<h1>Time Sectographer</h1>"
         };
-      });
+    });
 
 TaskListAddController.$inject = ['TaskListService'];
 function TaskListAddController(TaskListService) {
     var taskAdder = this;
+    taskAdder.value = "Lets Add Your ";
     taskAdder.taskName = '';
     taskAdder.taskDescription = '';
     taskAdder.taskTime = '';
@@ -24,6 +46,7 @@ function TaskListAddController(TaskListService) {
 TaskListShowController.$inject = ['TaskListService'];
 function TaskListShowController(TaskListService) {
     var showList = this;
+    showList.value = "Find ";
     showList.tasks = TaskListService.getTasks();
     showList.removeTask = function (taskIndex) {
         TaskListService.removeTask(taskIndex);
@@ -31,10 +54,10 @@ function TaskListShowController(TaskListService) {
 }
 function TaskListService() {
     var service = this;
-    var tasks = [{name:"Website Creation",description:"Develop Website Decently",category:"Work"}];
-    service.addTask = function (taskName, description, taskTime,category) {
+    var tasks = [{ name: "Website Creation", description: "Develop Website Decently", category: "Work" }];
+    service.addTask = function (taskName, description, taskTime, category) {
         //taskTime =$filter('date')(taskTime, "dd/MM/yyyy");
-        var task = { name: taskName, description: description, time: taskTime, category:category };
+        var task = { name: taskName, description: description, time: taskTime, category: category };
         tasks.push(task);
     };
     service.removeTask = function (taskIndex) {
